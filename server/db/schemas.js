c49cb0fac74a8befd4cb3d14ea0ebc218a35da1a
rolo <rodrigo.saavedra@screendrive.io>
2020-11-14 21:44:41 -0800

    Finished seed data generation, ready to write API to add it to database

--- server/db/schemas.js
+++ server/db/schemas.js
@@ -1,18 +1,27 @@
 var mongoose = require('mongoose');
 
-var itemSchema = mongoose.Schema({
+
+var productSchema = mongoose.Schema({
+    id: Number,
     department: String,
     itemName: String,
-    itemPhoto: String,
+    productPhoto: String,
     color: String,
-    price: Number,
-    size: String,
-    stock: Number, 
+    sizeType: String,
+    stock: [{
+        itemId: Number,
+        quantity: Number,
+        price: Number,
+        size: String
+    }],
     reviews: [{
-        stars: Number,
+        stars: String,
         description: String
     }]
 });
+
+
+//stretch goal
 var userSchema = mongoose.Schema({
     email: String,
     bag: [Number],
@@ -20,59 +29,59 @@ var userSchema = mongoose.Schema({
     request: [Number]
 });
 
-var itemModel = mongoose.model('Item', itemSchema);
+var productModel = mongoose.model('Item', itemSchema);
 var userModel = mongoose.model('User', userSchema);
 
 function findAll(query, callback) {
     if (query === 'items') {
-        itemModel.find({}, callback);
+        productModel.find({}, callback);
     } else if (query === 'users') {
         userModel.find({}, callback);
     }
 }
 
-function findItemsByName(itemName, callback) {
-    itemModel.find({ itemName: itemName }, callback);
+function findProductByName(itemName, callback) {
+    productModel.find({ itemName: itemName }, callback);
 }
 function findUserByEmail(email, callback) {
     userModel.find({ email: email }, callback);
 }
-function findItemById (id, callback) {
-    itemModel.find({_id : id}, callback);
+function findProductById(id, callback) {
+    productModel.find({ id: id }, callback);
 }
 //......................................
 // only to be used for seeding purposes:
-function insertOne(query, obj, itemId, callback) {
+function insertOne(query, obj, productId, callback) {
     if (query === 'item') {
-        itemModel.create(obj, callback);
+        productModel.create(obj, callback);
     } else if (query === 'user') {
         userModel.create(obj, callback);
     } else if (query === 'review') {
-        itemModel.findOneAndUpdate({ _id: itemId },
-            { $push: { review: obj } })
-    }  
+        itemModel.findOneAndUpdate({ id: productId },
+            { $push: { reviews: obj } })
+    }
 }
 //......................................
 
-function addItemID(basketName, itemID, email, callback) {
+function addProductId(basketName, productId, email, callback) {
     var query = { email: email }
     if (basketName === 'bag') {
         userModel.findOneAndUpdate({ email: email },
-            { $push: { bag: itemID } })
+            { $push: { bag: productId } })
     } else if (basketName === 'favorites') {
         userModel.findOneAndUpdate({ email: email },
-            { $push: { favorites: itemID } })
+            { $push: { favorites: productId } })
     } else {
         userModel.findOneAndUpdate({ email: email },
-            { $push: { request: itemID } })
+            { $push: { request: productId } })
     }
 
 }
 
 
 exports.findAll = findAll;
-exports.findItemsByName = findItemsByName;
+exports.findProductByName = findProductByName;
 exports.findUserByEmail = findUserByEmail;
-exports.findItemById = findItemById;
+exports.findProductById = findProductById;
 exports.insertOne = insertOne;
-exports.addItemID = addItemID;
+exports.addProductId = addProductId;
---
+++ server/db/seed.js
@@ -0,0 +1,76 @@
+var faker = require('faker');
+
+//array of imagelinks
+var imageLinks = [
+    `https://fecprojecthr.s3-us-west-1.amazonaws.com/Ultimate_Bra_(Plus_Size)_Black_FJ7342_21_model.jpg`,
+    `https://fecprojecthr.s3-us-west-1.amazonaws.com/Don't_Rest_Badge_of_Sport_Glam_On_Bra_(Plus_Size)_Pink_GL5854_21_model.jpg`,
+    `https://fecprojecthr.s3-us-west-1.amazonaws.com/Don't_Rest_Badge_of_Sport_Glam-On_Bra_Pink_GL5853_21_model.jpg`,
+    `https://fecprojecthr.s3-us-west-1.amazonaws.com/Down_Regen_Hooded_Blocked_Puffer_Jacket_Black_GE1324_21_model.jpg`,
+    `https://fecprojecthr.s3-us-west-1.amazonaws.com/Primeknit_Fine_Tights_Black_GD9404_21_model.jpg`,
+    `https://fecprojecthr.s3-us-west-1.amazonaws.com/Ultimate_Bra_(Plus_Size)_Black_FJ7342_21_model.jpg`
+];
+//array of sizes
+var sizes = [
+    `2XS-A`,`2XS`, `XS`, `S`, `M`, `L`, `XL`, `1X`, `2X`, `3X`, `4X`, `2XL`, 
+    `30"`, `32"`, `34`, `36"`, `38"`, `40"`, `42"`, `44"`, `46"`, `48"`
+];
+
+var random = (number) => (
+    Math.floor(Math.random() * number)
+);
+//create 100 items by running a 100x for loop
+var products = [];
+for (var i = 0; i < 100; i++) {
+    var product = {
+        id: i,
+        department: faker.commerce.department(),
+        itemName: faker.commerce.productName(),
+        productPhoto: imageLinks[random(imageLinks.length)],
+        color: faker.commerce.color(),
+        stock: [],
+        reviews: []
+    };
+    for (var j = 0; j < random(5); j++) {
+        product.reviews.push({
+            stars: random(5),
+            description: faker.commerce.productDescription()
+        })
+        product.stock.push({
+            itemId: random(1000),
+            quantity: random(20),
+            price: faker.commerce.price(),
+            size: sizes[random(sizes.length)]
+        })
+      
+    }
+    products.push(product);
+}
+console.log(products);
+console.log(products[0].reviews[0]);
+console.log(products[0].stock[0]);
+
+
+
+//insert into database
+
+
+//STRETCH GOAL
+// for (var i = 0; i < 100; i++) {
+//     var user = {
+//         email: faker.internet.email(),
+//         bag: [],
+//         favorites: [],
+//         request: []
+//     };
+//     for (var j = 0; j < random(5); j++) {
+//         var bfr = random(3);
+//         if (bfr === 0) {
+//             user.bag.push()//insert _id from randomly chosen item
+//         } else if (bfr === 1) {
+//             user.favorites.push()//insert _id from randomly chosen item
+//         } else {
+//             user.request.push()//insert _id from randomly chosen item
+//         }
+//     }
+// }
+
