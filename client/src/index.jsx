7a9fb7cdfcffdfd32b3e91db14ca0abb40cbac27
rolo <rodrigo.saavedra@screendrive.io>
2020-12-01 20:28:06 -0800

    changed service number to service 2

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './Components/App.jsx'
 
-ReactDom.render(<App />, document.getElementById('service1'));
+ReactDom.render(<App />, document.getElementById('service2'));
